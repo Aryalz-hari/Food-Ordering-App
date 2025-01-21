941b72fcdf5ec78f6abf1b1d76b8e26c8fc47522
Hari Aryal <aryalhari059@gmail.com>
2025-01-21 16:59:40 +0545

    modal

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,7 +8,7 @@ const Header = props =>{
  return (<Fragment>
     <header className={classes.header}>
         <h1>My Meals</h1>
-        <HeaderCartButton/>
+        <HeaderCartButton onClick={props.onShowCart}/>
     </header>
 <div className={classes['main-image']}>
     <img src={mealsImage} alt="A table of Food"></img>
